d2013a01fdde4f26377bcf20c34e2ddc3f9f2d9a
diane073 <127281732+diane073@users.noreply.github.com>
2023-06-20 17:34:08 +0900

    ✅ 테스트코드 작성 및 자잘한 오류 수정 (#42)
    * ✨ feed, comment model 생성
    
    * 🎨 model 이름 변경 : board->category
    
    * 💄 feed serializer, view 작성
    
    * 💄 urls 작성
    
    * 💄 like 기능 생성 및 약간의 코드 수정
    
    * 💄 community내 feed, comment search 추가
    
    * 🩹 feed model에 community 참조 추가
    
    * 💄 search url 추가
    
    * ✨ feed-community url 추가
    
    * 💄 feed 공지 등록 기능
    
    * ✨ 기능 테스트 및 코드수정
    
    * Update urls.py
    
    * ✨ list get test 성공!
    
    * ✨ 공지 기능 테스트 완료, feed list 코드 수정 밑 테스트 완료
    
    * 💄 공구 모델링
    
    * 💄 공구 model, serializer 생성
    
    * ✨ 공동구매 CRUD 구현 및 테스트 성공
    
    * 🚑 importerror hotfix
    
    * ✨ 공구 참여, 취소, 인원제한 구현 및 테스트 O
    
    * 💄 공구 close cron추가
    
    * 🎨 공지 기능 코드 개선
    
    * 🎨 list get 요소 추가, 자잘한 오류 수정
    
    * ⚡️ 피드 공지기능 개선
    
    * ⚡️ feed detail url 스펙대로 원복
    
    * ✨ 이전 글 다음 글 기능 구현
    
    * ⚡️ 공구 게시글 기능 추가
    
    * 🚑 import error
    
    * ✨ comment base model 작성 및 공구 comment 생성
    
    * ✨ 공구 시간데이터 유효성 확인, 게시글 상태메세지 구현
    
    * ⚡️ 종료된 공구게시글 삭제조건 추가
    
    * ✨공구: 유저 종료기능 구현
    
    * 🩹 http status 추가
    
    * ⚡️ 공구 meeting 시간 조건 추가
    
    * ✨ feed 테스트코드 작성 중
    
    * 중복 내용 삭제
    
    * 중복 내용 삭제
    
    * ⚡️ 코드 오류 수정
    
    * ✨ 테스트코드 작성 및 코드 정리
    
    ---------
    
    Co-authored-by: geunhye-kim <127708804+geunhye-kim@users.noreply.github.com>

--- feed/cron.py
+++ feed/cron.py
@@ -4,31 +4,6 @@ from django.utils import timezone
 from feed.models import GroupPurchase
 from .tasks import image_delete_job
 
-# class PurchaseCronJob(CronJobBase):
-#     pass
-# RUN_TIME = ["00:00"]
-# schedule = Schedule(run_at_times=RUN_TIME)
-# code = "feed.purchase_cron_job"
-
-# def do(self):
-#     six_months_ago = timezone.now() - timezone.timedelta(days=180)
-#     one_year_ago = timezone.now() - timezone.timedelta(days=365)
-#     five_years_ago = timezone.now() - timezone.timedelta(days=1825)
-
-#     dormant_users = User.objects.filter(last_login__lt=one_year_ago).update(
-#         is_dormant=True
-#     )
-#     if dormant_users:
-#         for user in dormant_users:
-#             pass
-
-#     User.objects.filter(withdraw_at__lt=five_years_ago).delete()
-
-#     user_pwd_expiry = User.objects.filter(change_password_at=six_months_ago)
-#     if user_pwd_expiry:
-#         for user in user_pwd_expiry:
-#             pass
-
 
 class ImageDeleteJob(CronJobBase):
     RUN_TIME = ["00:00"]
@@ -37,7 +12,8 @@ class ImageDeleteJob(CronJobBase):
 
     def do(self):
         image_delete_job.delay()
-        
+
+
 class MyPurchaseCronJob(CronJobBase):
     RUN_EVERY_MINS = 5
 
