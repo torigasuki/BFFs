2b57e63a28a645a4ed7674ec5a90340fd6eceed2
dana <bear9473@gmail.com>
2023-06-22 01:52:05 +0900

    ✨ AI친구 코드 구현 및 포스트맨 테스트O

--- meetai/admin.py
+++ meetai/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from meetai.models import AiChatBot
 
-# Register your models here.
+
+list_display = ["id", "user_id", "user_text", "ai_text"]
+
+search_fields = ["user_id"]
+ordering = ["user_id"]
+
+
+admin.site.register(AiChatBot)
--- meetai/models.py
+++ meetai/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class AiChatBot(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_text = models.JSONField(encoder=None, default=dict)
+    ai_text = models.JSONField(encoder=None, default=dict)
