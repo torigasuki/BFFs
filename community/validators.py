a63eb51632045ed3068db91b2b1cb5f827be56b7
dana <bear9473@gmail.com>
2023-06-28 20:45:51 +0900

    🎨 함수 네이밍 변경

--- community/validators.py
+++ community/validators.py
@@ -1,7 +1,7 @@
 import re
 
 
-def can_only_eng_and_int(value):
+def can_only_eng_int_underbar_and_hyphen(value):
     pattern = r"^[a-zA-Z][a-zA-Z0-9_-]{4,}$"
     if not re.match(pattern, value):
         return False
