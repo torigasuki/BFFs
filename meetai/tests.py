aa259f10c0dcf4159c6fdbf176fad2001c9d2a9a
diane073 <127281732+diane073@users.noreply.github.com>
2023-06-28 21:50:40 +0900

    🚑  커뮤니티 생성시 category id값삭제 (#71)
    * ✅ ai 옵션 조정 및 테스트코드 작성
    
    * 🎨 comments_count,정규식 조건 추가
    
    * 🎨 함수 네이밍 변경
    
    * 🚑 카테고리 id값 삭제!!!

--- meetai/tests.py
+++ meetai/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from user.models import User
+
+
+class UserProfileViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.user_data = {
+            "email": "test@test.com",
+            "password": "test12!@",
+            "name": "testuser",
+        }
+        cls.profile_data = {
+            "nickname": "testnick",
+            "introduction": "introduction",
+            "region": "seoul",
+        }
+        cls.user = User.objects.create_user(**cls.user_data)
+        cls.access_token = cls.client.post(reverse("login"), cls.user_data).data[
+            "access"
+        ]
+        cls.path = reverse("send_text_view")
+
+    def setUp(self):
+        self.user_input_data = {"user_input": "안녕!"}
+
+    def test_get_ai_chat(self):
+        response = self.client.get(
+            path=self.path,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_ai_chat_not_login(self):
+        response = self.client.get(
+            path=self.path,
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_post_ai_chat(self):
+        response = self.client.post(
+            path=self.path,
+            data=self.user_input_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+        )
+        self.assertEqual(response.status_code, 200)
