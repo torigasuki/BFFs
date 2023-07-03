fd499edd7b971ba267487d5c56435de339107b3f
Jeong SeungHo <teapackage16@gmail.com>
2023-07-03 18:58:54 +0900

    :recycle: Urlpatterns 정리

--- feed/urls.py
+++ feed/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .views import (
     FeedSearchView,
-    FeedAllView,
-    CommentView,
+    FeedListView,
+    FeedCreateView,
+    FeedDetailView,
+    FeedCategoryListView,
     CommentView,
     CocommentView,
-    CocommentView,
+    GroupPurchaseListView,
+    GroupPurchaseCreateView,
+    GroupPurchaseDetailView,
+    GroupPurchaseJoinedUserView,
+    GroupPurchaseSelfEndView,
     GroupPurchaseCommentView,
     ImageUploadAndDeleteView,
     FeedNotificationView,
@@ -14,9 +20,27 @@ from .views import (
 
 urlpatterns = [
     # feed 검색
-    path("search", FeedSearchView.as_view(), name="search_community_view"),
-    # 랜덤 커뮤 인기 feed list 조회
-    path("", FeedAllView.as_view(), name="feed_view"),
+    path("search", FeedSearchView.as_view(), name="search_feed_view"),
+    # feed 카테고리 list get
+    path(
+        "category/<str:category_url>/",
+        FeedCategoryListView.as_view(),
+        name="feed_category_list_view",
+    ),
+    # feed 전체 list get
+    path(
+        "list/",
+        FeedListView.as_view(),
+        name="feed_list_view",
+    ),
+    # feed 생성
+    path("feed/", FeedCreateView.as_view(), name="feed_create_view"),
+    # feed 상세, comment get / 수정 삭제
+    path(
+        "feed/<int:feed_id>/",
+        FeedDetailView.as_view(),
+        name="feed_detail_view",
+    ),
     # like 설정/취소
     path("<int:feed_id>/likes/", LikeView.as_view(), name="like_view"),
     # feed 게시글 공지 설정/취소
@@ -49,6 +73,42 @@ urlpatterns = [
         CocommentView.as_view(),
         name="cocomment_put_delete_view",
     ),
+    # grouppurchase 전체 list
+    path(
+        "grouppurchase/list/",
+        GroupPurchaseListView.as_view(),
+        name="grouppurchase_list_view",
+    ),
+    # grouppurchase 게시글 생성
+    path(
+        "grouppurchase/",
+        GroupPurchaseCreateView.as_view(),
+        name="grouppurchase_create_view",
+    ),
+    # grouppurchase 상세 페이지 / 수정 삭제
+    path(
+        "grouppurchase/<int:grouppurchase_id>/",
+        GroupPurchaseDetailView.as_view(),
+        name="grouppurchase_detail_view",
+    ),
+    # grouppurchase 참여 / 취소
+    path(
+        "grouppurchase/<int:grouppurchase_id>/join/",
+        GroupPurchaseJoinedUserView.as_view(),
+        name="grouppurchase_join_view",
+    ),
+    # grouppurchase 작성자 모집 끝 옵션
+    path(
+        "grouppurchase/<int:grouppurchase_id>/self_end/",
+        GroupPurchaseSelfEndView.as_view(),
+        name="purchase_self_end_view",
+    ),
+    # grouppurchase comment 생성
+    path(
+        "grouppurchase/<int:grouppurchase_id>/purchasecomment/",
+        GroupPurchaseCommentView.as_view(),
+        name="purchase_comment_create_view",
+    ),
     # grouppurchase comment 수정 삭제
     path(
         "purchasecomment/<int:purchase_comment_id>/",
@@ -56,4 +116,6 @@ urlpatterns = [
         name="purchase_comment_put_delete_view",
     ),
     path("image/upload/", ImageUploadAndDeleteView.as_view(), name="image_upload_view"),
+    # 랜덤 커뮤 인기 feed list 조회
+    # path("", FeedAllView.as_view(), name="feed_view"),
 ]
